e3572a39a145b218907aa9422d1f7d1de8dbeaf5
Andres Peña <elsenorcito@gmail.com>
2022-06-11 12:43:14 -0500

    base project in spring created

---
+++ src/test/java/com/prueba/nisumtest/NisumtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.nisumtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NisumtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
